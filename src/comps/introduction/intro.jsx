5f8cdc21d3a1a6657b09a8004ab5b72c197c4f9b
Lars <laarrss@protonmail.com>
2025-04-21 06:53:54 +0200

    ændringer

--- src/comps/introduction/intro.jsx
+++ src/comps/introduction/intro.jsx
@@ -9,14 +9,14 @@ export default function intro({ id, pic }) {
         <div className={styles.dual}>
           <div>
             <p>
-              Velkommen til min portfolio! Jeg er Lars Jul Vistisen, en
-              passioneret webudvikler med fokus på frontend-teknologier som
-              HTML, CSS, JavaScript og React. Jeg har en stærk interesse for at
-              skabe moderne, responsive og brugervenlige hjemmesider, der giver
-              en fremragende brugeroplevelse. Min rejse som webudvikler startede
-              med min uddannelse på Medieskolerne, hvor jeg har udviklet både
-              tekniske færdigheder og en dyb forståelse for webdesign og
-              interaktivitet.
+              Velkommen til min portfolio! Mit navn er Lars Jul Vistisen, og jeg
+              er en passioneret webudvikler med fokus på frontend-teknologier
+              som HTML, CSS, JavaScript og React. Jeg har en stærk interesse for
+              at skabe moderne, responsive og brugervenlige hjemmesider, der
+              giver en fremragende brugeroplevelse. Min rejse som webudvikler
+              startede da jeg meldte mig ind på webudvikler uddannelse på
+              Medieskolerne, hvor jeg har udviklet både tekniske færdigheder,
+              samt en dyb forståelse for webdesign.
             </p>
           </div>
           <div className={styles.imgBox}>
@@ -26,13 +26,9 @@ export default function intro({ id, pic }) {
         <p>
           I denne portfolio kan du finde eksempler på mine projekter, hvor jeg
           har anvendt mine færdigheder til at bygge funktionelle og æstetisk
-          tiltalende løsninger. Hver opgave afspejler min tilgang til
-          problemløsning, design og kodning, og hvordan jeg stræber efter at
-          levere højkvalitetsarbejde, der både er skalerbart og brugervenligt.
-        </p>
-        <p>
-          Jeg håber, du finder inspiration og værdi i mine projekter, og ser
-          frem til at samarbejde med jer på fremtidige webudviklingsprojekter.
+          tiltalende løsninger. Jeg håber, du finder inspiration og værdi i mine
+          projekter, og ser frem til at samarbejde med jer på fremtidige
+          webudviklingsprojekter.
         </p>
       </div>
     </>
--- src/main.jsx
+++ src/main.jsx
@@ -21,6 +21,14 @@ import { DiJavascript } from "react-icons/di";
 import { DiHtml5 } from "react-icons/di";
 import { DiCss3 } from "react-icons/di";
 
+//pics for cards.
+import projPic1 from "./assets/semesteropg.png";
+import projPic2 from "./assets/score.png";
+import projPic3 from "./assets/frem.png";
+
+//intro pic
+import cvPic from "./assets/cv.jpg";
+
 //Importere app comp, og viser det.
 //Filen her, bliver kørt i index.html.
 createRoot(document.getElementById("root")).render(
@@ -28,50 +36,39 @@ createRoot(document.getElementById("root")).render(
     <GeneralLayout>
       <Nav />
       <RightSec>
-        <Intro id={"intro"} pic={Pic} />
-        <CardSec title={"Projekt eksempler"} id={"projects"}>
+        <Intro id={"intro"} pic={cvPic} />
+        <CardSec title={"Eksempel projekter"} id={"projects"}>
           <Card
-            demoLink={"#"}
-            githubLink={"#"}
-            img={Pic}
+            demoLink={"https://lars007a.github.io/semesteropgave/"}
+            githubLink={"https://github.com/Lars007a/semesteropgave"}
+            img={projPic1}
             imgAlt={"Demo billed"}
             desc={
-              "Neque porro quisquam est qui dolorem ipsum quia dolor sit amet, consectetur. Neque porro quisquam est qui dolorem ipsum quia dolor sit amet, consectetur"
+              "Legekrogen, faktisk min semesteropgave, er en simpel hjemmeside til en fiktiv legetøjsbutik. Hjemmesiden er lavet som en semesteropgave under uddannelsen."
             }
-            title={"Projektnavn"}
+            title={"Legekrogen"}
             badges={["Javascript", "React", "HTML/CSS"]}
           />
           <Card
-            demoLink={"#"}
-            githubLink={"#"}
-            img={Pic}
+            demoLink={"https://lars007a.github.io/fremtidProjekt/"}
+            githubLink={"https://github.com/Lars007a/fremtidProjekt"}
+            img={projPic3}
             imgAlt={"Demo billed"}
             desc={
-              "Neque porro quisquam est qui dolorem ipsum quia dolor sit amet, consectetur. Neque porro quisquam est qui dolorem ipsum quia dolor sit amet, consectetur"
+              "En hjemmeside der er lavet til et skoleprojekt, hvor vi skulle skrive om nogle ting der ville ændre sig i fremtiden i forhold til teknologi og internettet."
             }
-            title={"Projektnavn"}
+            title={"Side for fremtidsprojekt"}
             badges={["Javascript", "React", "HTML/CSS"]}
           />
           <Card
-            demoLink={"#"}
-            githubLink={"#"}
-            img={Pic}
+            demoLink={"https://lars007a.github.io/scorepanel/"}
+            githubLink={"https://github.com/Lars007a/scorepanel/"}
+            img={projPic2}
             imgAlt={"Demo billed"}
             desc={
-              "Neque porro quisquam est qui dolorem ipsum quia dolor sit amet, consectetur. Neque porro quisquam est qui dolorem ipsum quia dolor sit amet, consectetur"
+              "Et projekt der viser et simpelt scorepanel til en håndboldkamp. Scorepanelet er ikke kun et design, men kan faktisk også tæle mål, tid, osv."
             }
-            title={"Projektnavn"}
-            badges={["Javascript", "React", "HTML/CSS"]}
-          />
-          <Card
-            demoLink={"#"}
-            githubLink={"#"}
-            img={Pic}
-            imgAlt={"Demo billed"}
-            desc={
-              "Neque porro quisquam est qui dolorem ipsum quia dolor sit amet, consectetur. Neque porro quisquam est qui dolorem ipsum quia dolor sit amet, consectetur"
-            }
-            title={"Projektnavn"}
+            title={"Scorepanel"}
             badges={["Javascript", "React", "HTML/CSS"]}
           />
         </CardSec>
@@ -79,38 +76,38 @@ createRoot(document.getElementById("root")).render(
         <SkillSec id={"skills"} title={"Færdigheder"}>
           <SkillCard
             desc={
-              "Neque porro quisquam est qui dolorem ipsum quia dolor sit amet, consectetur. Neque porro quisquam est qui dolorem ipsum quia dolor sit amet, consectetur"
+              "Jeg arbejder med React til at udvikle komponentbaserede webapplikationer. Jeg har erfaring med state management, props, hooks og brugergrænseflader, der er både funktionelle og nemme at vedligeholde."
             }
-            img={<FaReact size={30} />}
+            img={<FaReact size={30} color="61DBFB" />}
             imgAlt={"Pic"}
             title={"React"}
           />
 
           <SkillCard
             desc={
-              "Neque porro quisquam est qui dolorem ipsum quia dolor sit amet, consectetur. Neque porro quisquam est qui dolorem ipsum quia dolor sit amet, consectetur"
+              "JavaScript er mit primære værktøj til at skabe interaktive og dynamiske elementer på websider. Jeg har erfaring med alt fra DOM-manipulation til at bygge mindre applikationer med god kodepraksis."
             }
-            img={<DiJavascript />}
+            img={<DiJavascript color="F0DB4F" />}
             imgAlt={"Pic"}
-            title={"React"}
+            title={"Javascript"}
           />
 
           <SkillCard
             desc={
-              "Neque porro quisquam est qui dolorem ipsum quia dolor sit amet, consectetur. Neque porro quisquam est qui dolorem ipsum quia dolor sit amet, consectetur"
+              "Jeg bruger CSS til at skabe responsive og æstetiske brugerflader med fokus på layout, farver og typografi. Med SCSS arbejder jeg mere effektivt og struktureret gennem variabler, nesting og mixins."
             }
-            img={<DiCss3 size={30} />}
+            img={<DiCss3 size={30} color="264de4" />}
             imgAlt={"Pic"}
-            title={"React"}
+            title={"CSS"}
           />
 
           <SkillCard
             desc={
-              "Neque porro quisquam est qui dolorem ipsum quia dolor sit amet, consectetur. Neque porro quisquam est qui dolorem ipsum quia dolor sit amet, consectetur"
+              "Jeg har et solidt kendskab til semantisk og struktureret HTML, som danner fundamentet for alle mine webprojekter. Jeg fokuserer på tilgængelighed, klar struktur og et godt samarbejde med både CSS og JavaScript."
             }
-            img={<DiHtml5 size={30} />}
+            img={<DiHtml5 size={30} color="E34C26" />}
             imgAlt={"Pic"}
-            title={"React"}
+            title={"HTML"}
           />
         </SkillSec>
 
@@ -123,7 +120,7 @@ createRoot(document.getElementById("root")).render(
 
           <ContactCard
             title={"Email"}
-            info={"eksempel@eksempel.dk"}
+            info={"larsjulvistisen@gmail.com"}
             icon={<MdOutlineEmail size={60} />}
           />
         </ContactSec>
